8ced161ac8b1313294ed776552096ff993b8c681
SerxaS <118996747+SerxaS@users.noreply.github.com>
2024-08-29 19:54:19 +0300

    Initial restructure of crates.

---
+++ src/test.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod test {
+    use nova_scotia;
+
+    #[test]
+    fn nove_scotia() {
+        type G1 = pasta_curves::pallas::Point;
+        type G2 = pasta_curves::pallas::Point;
+
+        let circuit_file = root.join("circuit_files/nova_1.r1cs");
+    }
+}
